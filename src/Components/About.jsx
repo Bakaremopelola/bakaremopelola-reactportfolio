b7ab50fd05486f1b216a64f2a3efb3394b58720b
bakare mopelola <olorimaikudi@gmail.com>
2024-04-09 21:44:31 +0100

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,7 +15,7 @@ function About() {
         </div>
         <div className="aboutstory">
          
-          <p> Bakare Mopelola is a dedicated and skilled frontend web  <br />developer with a passion for crafting seamless   <br />and visually appealing user experiences. With a keen eye <br /> for design and a knack for coding,Mopelola has established   <br />herself as a proficient developer  <br />in the ever-evolving field of web development.</p>
+          <p> Bakare Mopelola is a dedicated and skilled frontend web  <br />developer with a passion for crafting seamless  <br />and visually appealing user experiences. With a keen eye <br /> for design and a knack for coding,Mopelola has established   <br />herself as a proficient developer  <br />in the ever-evolving field of web development.</p>
           <button className='bt2'>HIRE ME</button>
           <button className='bt1'>RESUME</button>
 
@@ -27,6 +27,6 @@ function About() {
       </>
     )
   }
-  
+       
   export default About;
   
