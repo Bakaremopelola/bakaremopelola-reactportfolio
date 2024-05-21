4168ab290a2f7c1d66520da62f4648b2dbed2b23
bakare mopelola <olorimaikudi@gmail.com>
2024-05-21 13:00:33 +0100

    new

---
+++ src/Components/Approuter.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Index from './Index'; 
+// import Projects from './Projects';
+// import Contact from './Contact'; 
+// import About from './About'; 
+// import Resume from './Resume'; 
+
+// const AppRouter = () => {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" component={Index} />
+//         <Route path="/projects" component={Projects} />
+//         <Route path="/contact" component={Contact} />
+//         <Route path="/about" component={About} />
+//         <Route path="/resume" component={Resume} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// export default AppRouter;
